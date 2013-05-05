2b0fb6f02ddcd19d6f3d433ca0e1b2a72ac85c09
Marcos Lin <linm.linmmac@ebacimail.com>
2013-05-05 16:53:01 +0200

    Added support to pass a file, or use - to indicate that data should be coming from STDIN

--- scripts/mongodb_init.sh
+++ scripts/mongodb_init.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+#
+# Author:  Marcos Lin
+# Created: 3 May 2013
+# 
+# This script is used to initialize the MongoDB's pyango database.
+# It accepts a JSON file as input arg, or pass - read from STDIN
+#
+
+json_file=$1
+default_file="../data/pyango.songs.json"
+
+help_needed() {
+	echo "Script used to load initial data to pyango MongoDB database.
+Usage:
+ $0 [<file>|-]
+
+Note:
+ * if <file> not passed, the default '$default_file' will be used.
+ * - is used to source the JSON from STDIN
+"
+}
+
+
+
+if [ "$json_file" = "--help" -o "$json_file" = "-h" ]; then
+	help_needed
+	exit
+elif [ -f "$json_file" ]; then
+	import_opt="--file $json_file"
+elif [ "$json_file" = "-" ]; then
+	import_opt=""
+else
+	import_opt="--file $default_file"
+fi
+
 mongo pyango --eval "db.dropDatabase()"
-mongoimport --db pyango --collection songs --file ../data/pyango.songs.json
+mongoimport --db pyango --collection songs $import_opt
