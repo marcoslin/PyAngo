40a83c0f67c11b3b8fcee648232415c5a95cbf76
Marcos Lin <linm.linmmac@ebacimail.com>
2013-05-07 19:52:43 +0200

    Allow options to be passed to the unit test script.

--- utest_run.sh
+++ utest_run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# To change target test browser, do:
+#   utest_run.sh --browsers Firefox
+
+
 # Do a single run for both unit and e2e tests
 cd utest
 
-
-./ang_run.sh --single-run
+./ang_run.sh --single-run $*
 if [ $? -ne 0 ]; then
 	echo "### Unit test failed."
 	exit 1
 fi
 
 echo ""
-./e2e_run.sh --single-run
+./e2e_run.sh --single-run $*
 if [ $? -ne 0 ]; then
 	echo "### End-to-end test failed."
 	exit 1
